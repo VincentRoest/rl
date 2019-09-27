23065e6bd3e9bf843c54eb008fe8306c64240b13
Gawan Dekker <gawan.dekker@gmail.com>
2019-09-27 22:21:50 +0200

    Merge.

--- lab2/windy_gridworld.py
+++ lab2/windy_gridworld.py
@@ -64,7 +64,6 @@ class WindyGridworldEnv(discrete.DiscreteEnv):
 
         for s in range(self.nS):
             position = np.unravel_index(s, self.shape)
-            # print(self.s)
             if self.s == s:
                 output = " x "
             elif position == (3,7):
